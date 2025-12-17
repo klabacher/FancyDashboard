8aeb57ee5edfe7e7491dbc267f50ebaa15d6a73e
JoãoPC <jvklabacher@gmail.com>
2025-12-17 01:25:17 -0300

    feat: refactor BentoGrid component to functional component with hooks

--- src/components/BentoGrid.tsx
+++ src/components/BentoGrid.tsx
@@ -1,69 +1,74 @@
-import React from "react";
+import { useEffect, useState, useCallback } from "react";
 import _ from "lodash";
 import RGL from "react-grid-layout";
 import { WidthProvider } from "react-grid-layout/legacy";
+import type * as RGLType from "react-grid-layout";
 
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
 
 const ReactGridLayout = WidthProvider(RGL);
 
-export default class ResizableHandles extends React.PureComponent {
-  static defaultProps = {
-    className: "bg-white/10 w-screen h-screen max-w-screen max-h-screen",
-    items: 20,
-    rowHeight: 30,
-    onLayoutChange: function () {},
-    cols: 12,
+// TODO: Criar default bento component and set max width and height to screen size math fuckery
+export default function BentoGrid({
+  className = "bg-white/10 w-screen h-screen max-w-screen max-h-screen",
+  items = 20,
+  rowHeight = 30,
+  cols = 12,
+  onLayoutChange = () => {},
+}: {
+  className?: string;
+  items?: number;
+  rowHeight?: number;
+  rowSize?: number;
+  cols?: number;
+  onLayoutChange?: (layout: RGLType.Layout) => void;
+}) {
+  const generateLayout = useCallback((): RGLType.Layout => {
+    const availableHandles = ["s", "w", "e", "n", "sw", "nw", "se", "ne"];
+
+    return Array.from({ length: items }, (_v, i) => {
+      const y = Math.ceil(Math.random() * 4) + 1;
+      return {
+        x: (i * 2) % cols,
+        y: Math.floor(i / 6) * y,
+        w: 2,
+        h: y,
+        i: i.toString(),
+        resizeHandles: availableHandles,
+      } as RGLType.LayoutItem;
+    }) as RGLType.Layout;
+  }, [items, cols]);
+
+  const [layout, setLayout] = useState<RGLType.Layout>(() => generateLayout());
+
+  useEffect(() => {
+    setLayout(generateLayout());
+  }, [generateLayout]);
+
+  const handleLayoutChange = (nextLayout: RGLType.Layout) => {
+    setLayout(nextLayout);
+    if (onLayoutChange) onLayoutChange(nextLayout);
   };
 
-  constructor(props: any) {
-    super(props);
-
-    const layout = this.generateLayout();
-    this.state = { layout };
-  }
-
-  generateDOM() {
-    return _.map(_.range(this.props.items), function (i) {
+  const generateDOM = useCallback(() => {
+    return _.map(_.range(items), function (i) {
       return (
         <div className="b-2 bg-amber-50 m-2" key={i}>
           <span className="text">{i}</span>
         </div>
       );
     });
-  }
+  }, [items]);
 
-  generateLayout() {
-    const p = this.props;
-    const availableHandles = ["s", "w", "e", "n", "sw", "nw", "se", "ne"];
-
-    return _.map(new Array(p.items), function (item, i) {
-      const y = _.result(p, "y") || Math.ceil(Math.random() * 4) + 1;
-      return {
-        x: (i * 2) % 12,
-        y: Math.floor(i / 6) * y,
-        w: 2,
-        h: y,
-        i: i.toString(),
-        resizeHandles: availableHandles,
-      };
-    });
-  }
-
-  onLayoutChange(layout) {
-    this.props.onLayoutChange(layout);
-  }
-
-  render() {
-    return (
-      <ReactGridLayout
-        layout={this.state.layout}
-        onLayoutChange={this.onLayoutChange}
-        {...this.props}
-      >
-        {this.generateDOM()}
-      </ReactGridLayout>
-    );
-  }
+  return (
+    <ReactGridLayout
+      layout={layout}
+      onLayoutChange={handleLayoutChange}
+      className={className}
+      gridConfig={{ cols, rowHeight }}
+    >
+      {generateDOM()}
+    </ReactGridLayout>
+  );
 }
